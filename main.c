be3a8b6ca3092909b533b5f4c20fc42637e0b512
IceTac <icetac2016@gmail.com>
2023-06-11 20:35:25 +0200

    Fourth Iteration

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main() {
-    printf("Hello, World!\n");
+    system("gnome-terminal -- ./TCP/TCPServer");
+    system("./TCP/TCPClient");
     return 0;
 }
